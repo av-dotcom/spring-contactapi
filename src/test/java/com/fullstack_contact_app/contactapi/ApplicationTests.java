4b58afb8b6609f73184b6d703598ad6434d6a170
AV <avilaca@av.local>
2024-07-18 19:18:58 -0400

    first commit

---
+++ src/test/java/com/fullstack_contact_app/contactapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack_contact_app.contactapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
