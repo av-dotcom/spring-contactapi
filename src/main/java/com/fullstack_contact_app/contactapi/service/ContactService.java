eb88ba91c1ac50be36e41e3257d346e36fce1637
AV <avilaca@av.local>
2024-07-20 10:49:37 -0400

    adding upload photo functionality pt 1

--- src/main/java/com/fullstack_contact_app/contactapi/service/ContactService.java
+++ src/main/java/com/fullstack_contact_app/contactapi/service/ContactService.java
@@ -9,6 +9,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 @Service
 @Slf4j
@@ -21,7 +32,7 @@ public class ContactService {
         return contactRepo.findAll(PageRequest.of(page, size, Sort.by("name")));
     }
 
-    public Contact findById(String id) {
+    public Contact findById(String id) { //this is getContact
         return contactRepo.findById(id).orElseThrow(() -> new RuntimeException("Contact not found"));
     }
 
@@ -39,6 +50,32 @@ public class ContactService {
 
     public void deleteContact(Contact contact) {
         contactRepo.delete(contact);
-        //TODO trying comment
     }
+
+    public String uploadPhoto(String id, MultipartFile file) {
+        log.info("Saving picture for user ID: {}", id);
+        Contact contact = findById(id);
+        String photoUrl = null;
+        contact.setPhotoUrl(photoUrl);
+        contactRepo.save(contact);
+        return photoUrl;
+    }
+
+    private final Function<String, String> fileExtension = filename -> Optional.of(filename).filter(name -> name.contains("."))
+            .map(name -> "." + name.substring(filename.lastIndexOf(".") + 1)).orElse(".png");
+
+    private final BiFunction<String, MultipartFile, String> photoFunction = (id, image) -> {
+        //String filename = id + fileExtension.apply(image.getOriginalFilename());
+        try {
+            Path fileStorageLocation = Paths.get("").toAbsolutePath().normalize();
+            if(!Files.exists(fileStorageLocation)) { Files.createDirectories(fileStorageLocation); }
+            Files.copy(image.getInputStream(), fileStorageLocation.resolve(id + ".png"), REPLACE_EXISTING);
+//            return ServletUriComponentsBuilder
+//                    .fromCurrentContextPath()
+//                    .path("/contacts/image/" + filename).toUriString();
+        } catch (Exception exception) {
+            throw new RuntimeException("Unable to save image");
+        }
+        return id;
+    };
 }
