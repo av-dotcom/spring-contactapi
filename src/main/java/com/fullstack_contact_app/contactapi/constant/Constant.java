3779381420c744843078620a41d0804985e8e017
avdotcom <me+github@avilaca.com>
2024-07-21 11:32:12 -0400

    small changes

--- src/main/java/com/fullstack_contact_app/contactapi/constant/Constant.java
+++ src/main/java/com/fullstack_contact_app/contactapi/constant/Constant.java
@@ -2,4 +2,5 @@ package com.fullstack_contact_app.contactapi.constant;
 
 public class Constant {
     public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads/";
+    public static final String X_REQUESTED_WITH = "X-Requested-With";
 }
